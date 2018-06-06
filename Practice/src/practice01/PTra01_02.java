06173900fd0d8b1434993fc3f1eb653493ce56fb
DESKTOP-DQNQRBK\likeit_student <yysuperjt@gmail.com>
2018-06-06 12:10:25 +0900

    1-2

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -10,10 +10,9 @@ public class PTra01_02 {
 	public static void main(String[] args) {
 
 		// ★ 12～14行目をコメントにしてください。
-		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
-
+		// ★「Hello, world」
+		// ★「Hello, world」
+		// ★「Hello, world」
 		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
 
 
