150270e009cfb89e4819a1c80eb8140d3a90e63f
DESKTOP-DQNQRBK\likeit_student <yysuperjt@gmail.com>
2018-06-11 17:51:07 +0900

    6-2

--- Practice/src/practice06/PTra06_02.java
+++ Practice/src/practice06/PTra06_02.java
@@ -11,14 +11,25 @@ public class PTra06_02 {
 
 		// ★ 20回繰り返しを行うfor文を記述してください
 		//-----for start
-
-			// ★ 今何回目の処理なのかを出力してください
-
-
+	
+			
+				
+				
+		
+					  
+				// ★ 今何回目の処理なのかを出力してください
+		for(int a=0;a<20;a++) {
+		
+			int i = a;
+					  
+						  
+			System.out.println(a+"回目="+i);
+						  
 			// ★ 奇数回のときには、「奇数」を出力してください
-
-
+			switch(a%2) {
+			case 0:
+				System.out.println("奇数→");
 
 		//-----for end
 	}
-}
+		}}}
