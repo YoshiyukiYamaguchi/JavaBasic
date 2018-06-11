c942a80983e0d4a3c4dd1415ff1bb4e81c645939
DESKTOP-DQNQRBK\likeit_student <yysuperjt@gmail.com>
2018-06-11 17:00:25 +0900

    5-4

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -23,16 +23,48 @@ public class PTra05_04 {
 		 */
 
 		// ①
-		System.out.println("0～6の整数を入力してください");
+		int i =0;
+		while (true) {
+				System.out.println("0～6の整数を入力してください");
+		
+				String line = scanner.nextLine();
+		
 
-		String line = scanner.nextLine();
+				switch(line) {
+					case "0":
+					System.out.println("日曜");
+					System.exit(0);System.exit(0);
+					break;
+					case "1":
+					System.out.println("月曜");
+					System.exit(0);System.exit(0);
+					break;
+					case "2":
+					System.out.println("火曜");
+					System.exit(0);System.exit(0);
+					break;
+					case"3":
+					System.out.println("水曜");
+					System.exit(0);System.exit(0);
+					break;
+					case"4":
+					System.out.println("木曜");
+					System.exit(0);System.exit(0);
+					break;
+					case"5":
+					System.out.println("金曜");
+					System.exit(0);System.exit(0);
+					break;
+					case"6":
+					System.out.println("土曜");
+					System.exit(0);System.exit(0);
+					default:
+					
+				
+				
 
-		switch(line) {
-			case "0":
-				System.out.println("日曜");
-				break;
-			case "1":
-				System.out.println("月曜");
-		}
+
 	}
-}
+		}
+	}}
+
