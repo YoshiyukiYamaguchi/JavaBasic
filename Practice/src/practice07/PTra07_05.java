0d02ce0b751814a16a45af62897bda6295eee12f
DESKTOP-EDO130Q\yysup <yysuperjt@gmail.com>
2018-06-21 18:06:24 +0900

    7-5 7-6 8-1

--- Practice/src/practice07/PTra07_05.java
+++ Practice/src/practice07/PTra07_05.java
@@ -12,6 +12,7 @@ public class PTra07_05 {
 		// ★ int型の配列arrayを宣言して、5個の領域を確保してください
 
 
+		int[] array = new int [5];
 		/*
 		 * ★ 下記の仕様でfor文を記述してください
 		 *
@@ -21,12 +22,17 @@ public class PTra07_05 {
 		 */
 		//------for start
 
-			// ★ 0～100000の中からランダムで値を取得して、配列arrayのi番目に代入してください
+		for(int i = 0; i < array.length; i++) {
+			array[i] = i +1;
 
+
+
+			// ★ 0～100000の中からランダムで値を取得して、配列arrayのi番目に代入してください
+			array[i] = new java.util.Random().nextInt(100000);
 
 			// ★ 配列arrayのi番目の値を出力してください
-
-
+			System.out.println(array[i]);
+		}
 		//------for end
 	}
 }
--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -14,8 +14,12 @@ public class PTra07_06 {
 				{500,40,6},
 				{70,8,90,1000}
 		};
+		int i = array[0][0] + array [0][1] + array [0][2] + array[1][3] + array[1][0] + array[1][1]
+				+array[1][2];
+
+
 
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
-
+		System.out.println(i/array.length);
 	}
 }
