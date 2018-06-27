33975399324563e0643c097d28c0c57d6c20a9b6
DESKTOP-EDO130Q\yysup <yysuperjt@gmail.com>
2018-06-27 17:15:37 +0900

    10-1

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -18,32 +18,15 @@ public class PTra10_01 {
 
 	public static void main(String[] args) {
 		User us = new User();
-		us.userId = 12345;
+		us.userId = 123;
+		us.userNm= "aaa";
+		us.mail = "aaa@";
+		us.password = "aaa123";
 		System.out.println(us.userId);
+		System.out.println(us.userNm);
+		System.out.println(us.mail);
+		System.out.println(us.password);
 
-		User us1 = new User ();
-		us1.userNm = "Yocchi";
-		System.out.println(us1.userNm);
-
-		User us2 = new User();
-		us2.mail = "aaa@bbb";
-		System.out.println(us2.mail);
-
-		User us3 = new User();
-		us3.password = "yocchi123";
-		System.out.println(us3.password);
-
-
-
-		}
-
-
-		// Userクラスを作成後に着手してください
-		// ★ User型の変数usを宣言してください
-
-		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
-
-
-		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
 
 	}
+		}
