c06adeac0631a439024a3ce19a111cf381c92be3
DESKTOP-EDO130Q\yysup <yysuperjt@gmail.com>
2018-06-27 15:11:02 +0900

    9-1 9-2

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -1,4 +1,5 @@
 package practice09;
+import practice09.util.Util;
 /*
  * PTra09_02.java
  *   作成	LIKEIT	2017
@@ -9,12 +10,25 @@ package practice09;
 public class PTra09_02 {
 	public static void main(String[] args) {
 		String[] array = {"Plain", "Island", "Mountain", "Swamp", "Forest"};
+		array = Util.shuffleArray(array);
+
+
 
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
 
 
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
+		for (int i = 0; i < array.length; i ++) {
+
+		System.out.println(array[i]);
+
+
 
 	}
 }
+}
+
+
+
+
