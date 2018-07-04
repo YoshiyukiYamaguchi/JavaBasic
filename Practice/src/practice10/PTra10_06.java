8e604a614b5fb7238bd467b06d3d6f86c21d0045
DESKTOP-EDO130Q\yysup <yysuperjt@gmail.com>
2018-07-04 17:01:02 +0900

    10-6

--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -13,11 +13,65 @@ public class PTra10_06 {
 	 */
 
 	public static void main(String[] args) {
+		Car[] car = new Car[3];
+		final int distance = 300;
+		int time = 0;
+		int d = 0;
+
+		Car car1 = new Car();
+		car1.color = "Red";
+		car1.gasoline = 50;
+
+		Car car2 = new Car();
+		car2.color = "Blue";
+		car2.gasoline = 400;
+
+		Car car3 = new Car();
+		car3.color = "Green";
+		car3.gasoline = 100;
+
+		car[0] = car1;
+		car[1] = car2;
+		car[2] = car3;
+
+
+		for ( int i = 0; i < car.length; i ++) {
+			int B = 0;
+			int n = 0;
+
+			while (true) {
+
+				int run = car[i].run();
+				n++;
+
+				B = B + run;
+
+
+				if (run == -1) {
+					System.out.println("目的地に到達できませんでした。");
+					break;
+				}
+
+				if (B > distance) {
+					System.out.println("目的地まで"	+ n + "時間かかりました。残りのガソリンは、" + car[i].gasoline +"リットルです");
+					break;
+				}
+
+			}
+		}
+
+
+
+
+/* B = 実行回数　　
+ * C = 進んだ距離*/
+
+
 
 		/*
 		 * carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
 		 * 各carインスタンスのrunメソッドを実行して、それぞれ「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください。
 		 */
-
 	}
 }
+
