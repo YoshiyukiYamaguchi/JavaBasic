98a0e9195b1b712065868007a35177d35ceb549d
DESKTOP-EDO130Q\yysup <yysuperjt@gmail.com>
2018-07-05 18:53:45 +0900

    11-1 11-2 11-3

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -1,20 +1,27 @@
 package practice11;
-/*
- * PTra11_02.java
- *   作成	LIKEIT	2017
- *------------------------------------------------------------
- * Copyright(c) Rhizome Inc. All Rights Reserved.
- */
+
+import java.util.Scanner;
 
 public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
+		Book[]A =FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		Scanner scanner = new Scanner(System.in);
+		String B = scanner.nextLine();
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+		System.out.println();
+		for (Book i : A) {
+			if (i.title.contains(B)) {
+				System.out.println(i.dispBookInfo());
+			}
+		}
+
+
+
 
 	}
 }
